338b33089004d1a2a6e5e059374499504915bdc4
CaptAlpha <arhitbose.tagore2020@vitstudent.ac.in>
2023-03-31 22:55:45 +0530

    Added Inital Files

---
+++ app.py
@@ -0,0 +1,13 @@
+# Flask Application
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+
+app = Flask(__name__)
+
+# Configurations
